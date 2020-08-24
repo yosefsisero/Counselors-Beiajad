c9d06fdb6b633160fbdbfd8d7ebe18d18cd3f461
yosef sisero <yosefsisero@gmail.com>
2020-08-24 16:28:10 -0300

    Se cambio el tamaño de texto

--- counselors-beiajad/src/Components/Seccion3/Seccion3.jsx
+++ counselors-beiajad/src/Components/Seccion3/Seccion3.jsx
@@ -11,12 +11,12 @@ function Seccion3() {
              <Row>
               <Col className="Col" md="12" lg={{ size: 6}}>
                  <h4 className="pregunta">¿Que es el Counseling?</h4>
-                 <h6 className="respuesta">
+                 <h5 className="respuesta">
                  El Conseling, traducida como Consultoría Psicológica, es una profesión de ayuda que promueve el desarrollo personal. Ofrece una relación y espacio cálido para facilitar el autoconocimiento, invitando al consultante a descubrirse y explorar un nuevo modo de ser y estar en el mundo para una mejor calidad de vida.
                  <br/><br/>
                  Así también, actúa en el área de la prevención y promueve el despliegue de las potencialidades. 
                  Se acompaña y preserva el bienestar psiquíco-emocional para una óptima relación intra e interpersonal; es decir, consigo mismo y con los demás.   
-                 </h6>          
+                 </h5>          
               </Col>
               <Col className="Col" md="12" lg={{ size: 6}}>
                  <img className="Foto" src={Foto1} alt=""/>
@@ -29,7 +29,7 @@ function Seccion3() {
               </Col>
               <Col className="Col" md="12" lg={{ size: 6}}>
                  <h4 className="pregunta">¿Cuándo debo considerar el Counseling como una opción?</h4>
-                 <h6 className="respuesta">
+                 <h5 className="respuesta">
                     Si estás atravesando cambios, conflictos, toma de decisiones, duelos, crisis vitales, dificultades... 
                     <br/>
                     Probá el counseling!!!
@@ -38,7 +38,7 @@ function Seccion3() {
                     <br/>
                     Nuestra ayuda esta dirigida a personas que necesitan un momento de escucha activa y contención emocional para lograr la mejor versión de sí mismos en cada momento de la vida y situación que se les presente.
                     <br/>
-                    Si necesitas vivir de una manera más satisfactoria y plena, contáctanos!!!</h6>           
+                    Si necesitas vivir de una manera más satisfactoria y plena, contáctanos!!!</h5>           
               </Col>
              </Row>
              
