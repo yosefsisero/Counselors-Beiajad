459fa652e898bf7c4f6370ea73920affc0b45902
yosef sisero <yosefsisero@gmail.com>
2020-08-24 13:28:04 -0300

    Solucionamois problemas

--- counselors-beiajad/src/Components/Layout/Footer/Footer.jsx
+++ counselors-beiajad/src/Components/Layout/Footer/Footer.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import './Footer.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import the library
+
+
+// import your icons
+
+import { faFacebook, faInstagram} from '@fortawesome/free-brands-svg-icons';
 
 
 function Footer() {
-
+    
     return (
         <>
          <div className="footerIzq">
              <h6>© 2020 Counselors Beyajad. Todos los derechos reservados</h6>
          </div>
          <div>
-         
-         
+         <FontAwesomeIcon icon={faFacebook} size="md"/>
+         <FontAwesomeIcon icon={faInstagram} />
          </div>
 
         </>
